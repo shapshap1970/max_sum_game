fdf5326c441d7b44548a984189a92dcba167750c
Avi Shapira <avis@ownbackup.com>
2024-04-08 08:29:23 +0300

    remove memiozation

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ def print_state(sum1, sum2, arr):
 def main():
     arr = [1,3,7,5,1, 2,5,9,5,4]
     sum1, sum2 = 0,0
-    memoization = {}
     while len(arr) >0:
         print_state(sum1, sum2, arr)
-        best_score, move = minmax(arr, 0 , 0 , True, {})
+        best_score, move = minmax(arr, 0 , 0 , True)
         print(f'best diff between computer and human is: {best_score}, computer is using {move}')
         if move == arr[0]:
             arr= arr[1:]
@@ -47,38 +46,31 @@ def get_sub_arrays(subs_arr):
 def get_hash(arr):
     return hash(tuple(arr))
 
-def minmax(subs_arr, sum1, sum2, is_max, memoization):
+def minmax(subs_arr, sum1, sum2, is_max):
     if len(subs_arr) ==0:
         calculated_score =  calc_score(sum1, sum2)
         return calculated_score, None
-    #if get_hash(subs_arr) in memoization:
-    #    return memoization[get_hash(subs_arr)]
+
     score = 0
     lr_move = None
     moves = get_sub_arrays(subs_arr)
     if is_max:
         max_score = -math.inf 
         for move in moves:
-                score, _ = minmax(move[1], sum1+move[0], sum2, not is_max, memoization)
+                score, _ = minmax(move[1], sum1+move[0], sum2, not is_max)
                 if max_score < score:
                     max_score = score
                     lr_move = move[0] 
 
-                    #memoization[get_hash(subs_arr)] = max_score, lr_move
         return max_score, lr_move
     else:
         min_score = math.inf
         for move in moves:
-            score, _ = minmax(move[1], sum1, sum2+move[0], not is_max, memoization)
+            score, _ = minmax(move[1], sum1, sum2+move[0], not is_max)
             if min_score > score:
                 min_score = score
                 lr_move = move[0]
         return min_score, lr_move
-                #memoization[get_hash(subs_arr)] = min_score, lr_move
-    #return memoization[get_hash(subs_arr)]
-
-
-
 
 
 if __name__ == '__main__':
